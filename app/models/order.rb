5f945b48b134c6a3b36dee7db6d1621a07cb34f6
Ahmed Aziz <ahm.aziz12@gmail.com>
2024-01-10 06:05:25 +0500

    added query

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :user
 
+  validates :price, :quantity, :order_type, presence: true
+
   enum status: [:pending, :completed, :cancelled]
   enum order_type: [:buy, :sell]
 
   scope :completed_buy, -> { where(status: :completed, order_type: :buy) }
+
+  # Query asked in demo
+  # Order.where(user_id: 1, status: 'completed').sum(:quantity)
 
   def self.process_order(order)
     if order.buy?
